c856bfaf2ad051c2b2e1740e00fbe81f46ef15b6
Angel Figueroa <angelnewrg4l@outlook.com>
2022-09-11 11:13:17 -0500

    Reducer para form

--- midudev-tsreact/src/components/Form.tsx
+++ midudev-tsreact/src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useReducer, useState } from "react"
 import {Sub} from "../interfaces/Sub";
 interface FormState {
     inputValues : Sub
@@ -8,25 +8,62 @@ interface PropsDeForm {
     onNewSub: (newSub:Sub)=> void
 }
 
+type FormReducerAction = {
+    type: "change_value",
+    payload: {
+        inputName:string,
+        inputValue:string
+    }
+} | {
+    type: "clear"
+}
+
+const initial_state = {
+    nickname:'',
+    months:0,
+    avatar:'https://i.pravatar.cc/150?img=',
+    description:''
+}
+
+const formReducer = (state:FormState["inputValues"], action:FormReducerAction)=>{
+    switch(action.type){
+        case "change_value":
+            return{
+                ...state,
+                [action.payload.inputName]: action.payload.inputValue
+            }
+        case "clear":
+            return initial_state
+    }
+}
+
 const Form = ({onNewSub}:PropsDeForm) => {
 
-    const [inputValues, setInputValues] = useState<FormState["inputValues"]>({
+    /* const [inputValues, setInputValues] = useState<FormState["inputValues"]>({
         nickname:'',
         months:0,
         avatar:'https://i.pravatar.cc/150?img=',
         description:''
-    })
+    }) */
+
+    const [inputValues, dispatch] = useReducer(formReducer, initial_state)
 
     const handleChange = (evt:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)=>{
-        setInputValues({
+        /* setInputValues({
             ...inputValues,
             [evt.target.name]: evt.target.value
-        })
+        }) */
+        dispatch({type:"change_value", payload:{inputName:evt.target.name,inputValue: evt.target.value}})
     }
 
     const handleSubmit = (evt:React.FormEvent<HTMLFormElement>) =>{
         evt.preventDefault();
         onNewSub(inputValues)
+        handleClear()
+    }
+
+    const handleClear = ()=>{
+        dispatch({type: "clear"})
     }
 
   return (
